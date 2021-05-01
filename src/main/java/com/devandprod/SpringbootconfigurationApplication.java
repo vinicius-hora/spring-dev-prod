18eee9152ed05b9cab964e4ee7e8525b626e1c39
vinicius-hora <vinicius_hora@live.com>
2021-05-01 12:42:01 -0300

    ambiente de configuração dev e producao

---
+++ src/main/java/com/devandprod/SpringbootconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.devandprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootconfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devandprod/SpringbootconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devandprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
